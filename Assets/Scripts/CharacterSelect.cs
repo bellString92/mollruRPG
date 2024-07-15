bf3ba8a053dc7e06d656259bbd8c257a15bb372c
bellString92 <158009452+bellString92@users.noreply.github.com>
2024-07-15 12:53:41 +0900

    Player Prefabs 닉네임 추가

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -10,6 +10,7 @@ using UnityEngine.UI;
 public class CharacterSelect : MonoBehaviour
 {
     public int startNum = 0;
+    private string _nickName = "";
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
@@ -26,13 +27,17 @@ public class CharacterSelect : MonoBehaviour
     public void OnCreateSelectSlotClick(int num)
     {
         if (GameObject.Find($"Paladin{num}") == null)
-            CreatePopup(PopupType.CREATE, num);
+            CreateNickName(PopupType.CREATE, num);
         else
-            OnCreateSelectSlotClickOk(num);
+        {
+            string nickName = GameObject.Find($"NickName{num}").transform.GetComponentInChildren<TMPro.TMP_Text>().text;
+            OnCreateSelectSlotClickOk(num, nickName);
+        }
     }
 
-    private void OnCreateSelectSlotClickOk(int num)
+    private void OnCreateSelectSlotClickOk(int num, string nickName = "")
     {
+        _nickName = nickName;
         GameObject selSlot = GameObject.Find($"Slot{num}");
         for (int i = 1; i <= 3; i++)
         {
@@ -49,12 +54,17 @@ public class CharacterSelect : MonoBehaviour
 
         if (GameObject.Find($"Paladin{num}") == null)
         {
-            GameObject prefab = Resources.Load("Prefabs/Paladin") as GameObject;
-            GameObject paladin = Instantiate(prefab);
+            GameObject paladin = Instantiate(Resources.Load("Prefabs/Paladin") as GameObject);
             paladin.name = $"Paladin{num}";
-            paladin.transform.parent = selSlot.transform;
+            paladin.transform.SetParent(selSlot.transform);
             paladin.transform.localPosition = Vector3.zero;
             paladin.transform.localRotation = Quaternion.identity;
+        }
+
+        if (!"".Equals(nickName))
+        {
+            GameObject.Find($"NickName{num}").transform.GetChild(0).gameObject.SetActive(true);
+            GameObject.Find($"NickName{num}").transform.GetComponentInChildren<TMPro.TMP_Text>().text = nickName;
         }
 
         GameObject.Find("CreateSelectSlot").transform.GetComponent<Transform>().Find($"Slot{num}Btn").GetComponentInChildren<TMPro.TMP_Text>().text = "캐릭터 선택";
@@ -81,10 +91,12 @@ public class CharacterSelect : MonoBehaviour
             }
         }
         Destroy(GameObject.Find($"Paladin{num}"));
+
+        GameObject.Find($"NickName{num}").transform.GetChild(0).gameObject.SetActive(false);
+
         GameObject.Find("CreateSelectSlot").transform.GetComponent<Transform>().Find($"Slot{num}Btn").GetComponentInChildren<TMPro.TMP_Text>().text = "신규캐릭터 생성";
         GameObject.Find("DeleteSlot").transform.GetComponent<Transform>().Find($"Slot{num}Btn").transform.gameObject.SetActive(false);
-        if (num == startNum)
-            startNum = 0;
+        if (num == startNum) startNum = 0;
     }
 
     public void GameStart()
@@ -96,40 +108,109 @@ public class CharacterSelect : MonoBehaviour
     {
         PlayerPrefs.SetString("nextSceneText", "마을");
         PlayerPrefs.SetString("nextSceneImage", "MollRu_Town");
+        PlayerPrefs.SetString("nickName", _nickName);
         SceneChange.OnSceneChange("MollRuRPGScene");
     }
 
-    private void CreatePopup(PopupType type, int num = 0)
+    private void CreateNickName(PopupType type, int num)
     {
-        GameObject prefabs = Resources.Load("Prefabs/Popup") as GameObject;
-        GameObject popup = Instantiate(prefabs) as GameObject;
+        GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(true);
+        GameObject textPopup = Instantiate(Resources.Load("Prefabs/TextPopup") as GameObject);
+
+        textPopup.transform.GetChild(1).GetComponentInChildren<TMPro.TMP_Text>().text = "닉네임 설정";
+        textPopup.transform.GetChild(3).GetComponentInChildren<TMPro.TMP_Text>().text = "확인";
+        textPopup.transform.GetComponentInChildren<Button>().onClick.AddListener(() => {
+            string nickName = textPopup.transform.GetChild(2).GetComponentInChildren<TMPro.TMP_InputField>().text;
+            string result = NickNameCheck(nickName);
+            if ("".Equals(result))
+            {
+                textPopup.transform.GetChild(2).GetChild(1).transform.gameObject.SetActive(false);
+                Destroy(textPopup);
+                GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(false);
+                CreatePopup(type, num, nickName);
+            }
+            else
+            {
+                textPopup.transform.GetChild(2).GetChild(1).GetComponent<TMPro.TMP_Text>().text = result;
+                textPopup.transform.GetChild(2).GetChild(1).transform.gameObject.SetActive(true);
+            }
+        });
+        
+        textPopup.transform.SetParent(GameObject.Find("NickName").transform);
+        textPopup.transform.localPosition = Vector3.zero;
+        textPopup.SetActive(true);
+
+    }
+
+    private string NickNameCheck(string nickName)
+    {
+        if ("".Equals(nickName))
+        {
+            return "닉네임을 입력해주세요.";
+        }
+
+        if (nickName.Length > 10)
+        {
+            return "닉네임은 최대 10글자 입니다.";
+        }
+
+        if (nickName.Contains(" "))
+        {
+            return "띄어쓰기는 불가능합니다.";
+        }
+
+        return "";
+        
+    }
+
+    private void CreatePopup(PopupType type, int num = 0, string nickName = "")
+    {
+        GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(true);
+        GameObject popup = Instantiate(Resources.Load("Prefabs/Popup") as GameObject);
         TMPro.TMP_Text[] texts = popup.transform.GetComponentsInChildren<TMPro.TMP_Text>();
 
         Button[] btns = popup.transform.GetComponentsInChildren<Button>();
-        btns[0].onClick.AddListener(() => { Destroy(popup); });
+        btns[0].onClick.AddListener(() => {
+            Destroy(popup);
+            GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(false);
+        });
         switch (type)
         {
             case PopupType.CREATE:
                 texts[0].text = "생성 하시겠습니까?";
-                btns[1].onClick.AddListener(() => { OnCreateSelectSlotClickOk(num); Destroy(popup); });
+                btns[1].onClick.AddListener(() => {
+                    OnCreateSelectSlotClickOk(num, nickName);
+                    Destroy(popup);
+                    GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(false);
+                });
                 break;
             case PopupType.DELETE:
                 texts[0].text = "삭제 하시겠습니까?";
-                btns[1].onClick.AddListener(() => { OnDeleteSlotClickOk(num); Destroy(popup); });
+                btns[1].onClick.AddListener(() => {
+                    OnDeleteSlotClickOk(num); Destroy(popup);
+                    GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(false);
+                });
                 break;
             case PopupType.START:
                 if (num == 0)
                 {
                     texts[0].text = "캐릭터를 선택해주세요.";
-                    btns[1].onClick.AddListener(() => { Destroy(popup); });
+                    btns[1].onClick.AddListener(() => {
+                        Destroy(popup);
+                        GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(false);
+                    });
                 } else
                 {
                     texts[0].text = "게임 시작하시겠습니까?";
-                    btns[1].onClick.AddListener(() => { GameStartOk(num); Destroy(popup); });
+                    btns[1].onClick.AddListener(() => {
+                        GameStartOk(num);
+                        Destroy(popup);
+                        GameObject.Find("DontTouch").transform.GetChild(0).gameObject.SetActive(false);
+                    });
                 }
                 break;
         }
-        popup.transform.parent = GameObject.Find("PopupParent").transform;
+        popup.transform.SetParent(GameObject.Find("PopupParent").transform);
         popup.transform.localPosition = Vector3.zero;
         popup.SetActive(true);
     }
---
+++ Assets/Scripts/NickName.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NickName : MonoBehaviour
+{
+    public GameObject Cam;
+    Vector3 startScale;
+    public float distance = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string nickName = PlayerPrefs.GetString("nickName");
+        if ("".Equals(nickName)) nickName = "´Ð³×ÀÓ";
+        gameObject.GetComponent<TMPro.TMP_Text>().text = nickName;
+        gameObject.GetComponent<TMPro.TMP_Text>().outlineColor = Color.white;
+        gameObject.GetComponent<TMPro.TMP_Text>().outlineWidth = 0.05f;
+        startScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = Cam.transform.rotation;
+        float dist = Vector3.Distance(Cam.transform.position, transform.position);
+        Vector3 newScale = startScale * dist / distance;
+        transform.localScale = newScale;
+
+        transform.rotation = Cam.transform.rotation;
+    }
+}
