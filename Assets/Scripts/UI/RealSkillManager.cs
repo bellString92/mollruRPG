1e2b54d3e14fd8c065d4d3fe083ef6a723203861
DHKim074 <jjso6513@naver.com>
2024-08-13 12:08:50 +0900

    중간저장

---
+++ Assets/Scripts/UI/RealSkillManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RealSkillManager : MonoBehaviour
+{
+    List<Image> mySkill = new List<Image>();                // 스킬 이미지 가져오기
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
